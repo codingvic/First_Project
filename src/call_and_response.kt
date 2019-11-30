aabec30f6a8e52b72875d90f84ad480cb1cdde84
coding_vic <victor.dossin@gmail.com>
2019-11-29 19:11:02 -0800

    simple number function, easy errors

--- src/call_and_response.kt
+++ src/call_and_response.kt
@@ -1,3 +1,11 @@
 fun main() {
-    println("Hello Repository!")
+    println("Please enter a number")
+    var num1 = readLine().toString().toInt()
+    println("Please enter another number")
+    var num2 = readLine().toString().toInt()
+    println(doMath(num1,num2))
+}
+
+fun doMath(num1:Int, num2:Int):Int {
+    return num1 + num2
 }
